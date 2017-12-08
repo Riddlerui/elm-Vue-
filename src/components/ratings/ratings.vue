3151e068ac88315e525d71adb225c744772b2211
xurui <xurui@wzdai.com>
2017-12-08 15:33:11 +0800

    第一次提交:header1

---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>ratings</h3>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+
+}
+</script>
+<style lang="stylus">
+
+</style>
---
+++ src/components/seller/seller.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>seller</h3>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
