acb0c40f2cea98234d342a139c4056d3b70cd874
xurui <xurui@wzdai.com>
2017-12-11 11:58:27 +0800

    组件header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -16,10 +16,18 @@
             <span class="text">{{ seller.supports[0].description }}</span> 
           </div>
         </div>
+        <div class="support-count" v-if="seller.supports">
+          <span class="count">{{ seller.supports.length }}个</span>
+          <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+          <img :src="seller.avatar" width="100%" height="100%" alt="">
+        </div>
       </div>
     </div>
     <div class="bulletin-wrapper">
-
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
     </div>
   </div>
 </template>
@@ -38,41 +46,44 @@
 <style lang="stylus">
 @import "../../common/stylus/mixin";
   .header
-    color : #fff
-    background : #000
+    color #fff
+    background-color rgba(7, 17, 27 .5)
+    position relative
     .content-wrapper
-      padding : 24px 12px 18px 24px
-      font-size : 0
+      position relative
+      padding 24px 12px 18px 24px
+      font-size 0
       .avatar
-        display : inline-block
-        vertical-align : top
+        display inline-block
+        vertical-align top
         img
-          border-radius : 2px
+          border-radius 2px
       .content
-        display : inline-block  
-        margin-left : 16px
+        display inline-block  
+        margin-left 16px
         .title
-          margin : 2px 0 8px 0
+          margin 2px 0 8px 0
           .brand
-            width : 30px
-            height : 18px
-            display : inline-block
+            width 30px
+            height 18px
+            display inline-block
             bg-image('brand')
-            background-size : 30px 18px
-            background-repeat : no-repeat
-            vertical-align : top
+            background-size 30px 18px
+            background-repeat no-repeat
+            vertical-align top
           .name
-            margin-left : 6px
-            font-size : 16px
+            margin-left 6px
+            font-size 16px
             font-weight bold
-            line-height : 18px
+            line-height 18px
         .description
-          margin-bottom : 10px
-          line-height : 12px
-          font-size : 12px
+          margin-bottom 10px
+          line-height 12px
+          font-size 12px
         .support
           .icon
             display inline-block
+            vertical-align top
             width 12px
             height 12px
             margin-right 4px
@@ -88,6 +99,62 @@
               bg-image('invoice_1')
             $.special
               bg-image('special_1')
+          .text
+            line-height 12px
+            font-size 10px
+      .support-count
+        position absolute
+        right 12px
+        bottom  14px
+        padding 0 8px
+        height 24px
+        line-height 24px
+        border-radius 14px
+        background rgba(0, 0, 0, 0.2)     
+        text-align center
+        .count
+          font-size 10px
+          vertical-align top
+        .icon-keyboard_arrow_right
+          line-height 24px
+          font-size 10px
+          margin-right 2px
 
+    .bulletin-wrapper
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      position relative
+      background rgba(7, 17, 27, .2)
+      .bulletin-title
+        display inline-block
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+        vertical-align top
+        margin-top 7px
+      .bulletin-text
+        font-size 10px
+        font-weight 200
+        margin 0 4px
+        vertical-align top
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top 8px
+    .background
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index -1
+      filter blur(10px)
 </style>
 
